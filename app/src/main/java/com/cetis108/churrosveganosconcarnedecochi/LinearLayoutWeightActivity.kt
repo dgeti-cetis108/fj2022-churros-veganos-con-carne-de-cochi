934da5c9fe1c7e92ac7c01a89df2b11a29de47af
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-04-05 13:25:39 -0600

    Layouts
    Delete constraint layout, and use space element to send button to the bottom of layout

---
+++ app/src/main/java/com/cetis108/churrosveganosconcarnedecochi/LinearLayoutWeightActivity.kt
@@ -0,0 +1,11 @@
+package com.cetis108.churrosveganosconcarnedecochi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutWeightActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout_weight)
+    }
+}
